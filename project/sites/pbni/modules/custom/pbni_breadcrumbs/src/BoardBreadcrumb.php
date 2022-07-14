cd8b743ba98352d308814496aae5efac3f51f069
OMAHM <hello@onemanandhismouse.com>
2022-07-14 19:42:13 +0100

    Additional test install

---
+++ project/sites/pbni/modules/custom/pbni_breadcrumbs/src/BoardBreadcrumb.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace Drupal\pbni_breadcrumbs;
+
+/**
+ * @file
+ * Generates the breadcrumb trail for content including:
+ * - Board member
+ *
+ * In the format:
+ * > Home
+ * > About Probation
+ * > Board
+ * > current-page-title
+ *
+ * > <front>
+ * > /about-probation
+ * > /board
+ * > /current-page-title
+ */
+use Drupal\Core\Breadcrumb\Breadcrumb;
+use Drupal\Core\Breadcrumb\BreadcrumbBuilderInterface;
+use Drupal\Core\Controller\TitleResolverInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+use Drupal\node\NodeInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * {@inheritdoc}
+ */
+class BoardBreadcrumb implements BreadcrumbBuilderInterface {
+
+  /**
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Node object, or null if on a non-node page.
+   *
+   * @var \Drupal\node\Entity\Node
+   */
+  protected $node;
+
+  /**
+   * The title resolver.
+   *
+   * @var \Drupal\Core\Controller\TitleResolverInterface
+   */
+  protected $titleResolver;
+
+  /**
+   * Symfony\Component\HttpFoundation\RequestStack definition.
+   *
+   * @var Symfony\Component\HttpFoundation\RequestStack
+   */
+  protected $request;
+
+  /**
+   * Class constructor.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, TitleResolverInterface $title_resolver, RequestStack $request) {
+    $this->entityTypeManager = $entity_type_manager;
+    $this->titleResolver = $title_resolver;
+    $this->request = $request;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager'),
+      $container->get('title_resolver'),
+      $container->get('request_stack')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function applies(RouteMatchInterface $route_match) {
+    $match = FALSE;
+    $route_name = $route_match->getRouteName();
+    if ($route_name == 'entity.node.canonical') {
+      $this->node = $route_match->getParameter('node');
+      if ($this->node instanceof NodeInterface == FALSE) {
+        $this->node = $this->entityTypeManager->getStorage('node')->load($this->node);
+      }
+      if (!empty($this->node)) {
+        if ($this->node->bundle() == 'board_member') {
+          $match = TRUE;
+        }
+      }
+    }
+    return $match;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(RouteMatchInterface $route_match) {
+    $breadcrumb = new Breadcrumb();
+    $type_resolver = $this->node->get('field_type')->getValue($this->request->getCurrentRequest(),
+        $route_match->getRouteObject())[0]['value'];
+    $title_resolver = $this->titleResolver->getTitle($this->request->getCurrentRequest(), $route_match->getRouteObject());
+    if ($this->node) {
+      $links[] = Link::createFromRoute(t('Home'), '<front>');
+      if (Url::fromUri('internal:/about-probation')->isRouted()) {
+        $links[] = Link::fromTextandUrl(t('About Probation'), Url::fromUri('internal:/about-probation'));
+      }
+
+      if ($type_resolver == 'board_member') {
+        if (Url::fromUri('internal:/board')->isRouted()) {
+          $links[] = Link::fromTextandUrl(t('The Board'), Url::fromUri('internal:/board'));
+        }
+      }
+      elseif ($type_resolver == 'senior_management') {
+        if (Url::fromUri('internal:/senior-management')->isRouted()) {
+          $links[] = Link::fromTextandUrl(t('Senior management'), Url::fromUri('internal:/senior-management'));
+        }
+      }
+      $links[] = Link::createFromRoute($title_resolver, '<none>');
+      $breadcrumb->setLinks($links);
+    }
+    $breadcrumb->addCacheContexts(['url.path']);
+    return $breadcrumb;
+  }
+
+}
