ea7fac3e8e6e7a3728cc3ffffd9905679ba18e21
adammcfet <adam.mcfetridge@finance-ni.gov.uk>
2022-08-22 15:35:09 +0100

    PR tidy up

--- project/sites/imtac/settings.local.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$config['config_split.config_split.local']['status'] = FALSE;
-$config['config_split.config_split.hosted']['status'] = FALSE;
-$config['config_split.config_split.production']['status'] = TRUE;
