2dafef496ca2eb400dd90d928c6299e4b11b0091
adammcfet <adam.mcfetridge@finance-ni.gov.uk>
2022-08-22 13:56:12 +0100

    Config split set up

---
+++ project/sites/imtac/settings.local.php
@@ -0,0 +1,5 @@
+<?php
+
+$config['config_split.config_split.local']['status'] = FALSE;
+$config['config_split.config_split.hosted']['status'] = FALSE;
+$config['config_split.config_split.production']['status'] = TRUE;
