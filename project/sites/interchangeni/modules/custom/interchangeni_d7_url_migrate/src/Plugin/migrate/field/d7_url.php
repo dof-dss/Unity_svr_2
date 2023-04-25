8070ec5c42bc3960eec383f6ae8563623b3b6b14
Adam Williamson <adam.williamson@finance-ni.gov.uk>
2023-04-25 08:33:50 +0100

    D7 Url migration module

---
+++ project/sites/interchangeni/modules/custom/interchangeni_d7_url_migrate/src/Plugin/migrate/field/d7_url.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Drupal\interchangeni_d7_url_migrate\Plugin\migrate\field;
+
+use Drupal\migrate\Plugin\MigrationInterface;
+use Drupal\migrate_drupal\Annotation\MigrateField;
+use Drupal\migrate_drupal\Plugin\migrate\field\FieldPluginBase;
+
+/**
+ * MigrateField Plugin for Drupal 6 and 7 email fields.
+ *
+ * @MigrateField(
+ *   id = "d7_url",
+ *   core = {7},
+ *   type_map = {
+ *     "url" = "link"
+ *   },
+ *   source_module = "url",
+ *   destination_module = "link"
+ * )
+ */
+class d7_url extends FieldPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFieldWidgetMap() {
+    return [
+      'url_external' => 'link_default',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFieldFormatterMap() {
+    return [
+      'url_default' => 'link',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function processFieldValues(MigrationInterface $migration, $field_name, $data) {
+    $process = [
+      'plugin' => 'sub_process',
+      'source' => $field_name,
+      'process' => [
+        'uri' => 'value',
+      ],
+    ];
+    $migration->setProcessOfProperty($field_name, $process);
+  }
+
+}
