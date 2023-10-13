4f7fbd6c0098b1f53e7e7e6ecbc9c488df8a74ba
Johan Gant <johan.gant@gmail.com>
2023-10-13 10:01:38 +0100

    Add updated script to take sites to build as parameters (#1055)
    * maestro upgrade
    
    * Add updated script to take sites to build as parameters
    
    ---------
    
    Co-authored-by: DuttonMa <martin@theduttons.org>

--- build-themes.sh
+++ build-themes.sh
@@ -1,24 +1,38 @@
 #!/usr/bin/env bash
+
 echo "Starting unity theme update"
 
 # Define the path to the 'sites' directory
-sites_dir=${1:-"/app/project/sites"}
+sites_dir="/app/project/sites"
 
-# Loop through each directory in the 'sites' directory
-for site in "$sites_dir"/*; do
-    site_name=$(basename "$site")
-    themes_dir="$site/themes/${site_name}_theme"
+if [ "$#" -gt 0 ]; then
+    # If site names are provided, process them
+    IFS=',' read -r -a site_names <<< "$1"
+else
+    # If no site names are provided, get all site names
+    site_names=($(ls -1 "$sites_dir"))
+fi
+
+for site_name in "${site_names[@]}"; do
+    # Trim leading and trailing whitespace from site name
+    site_name=$(echo "$site_name" | xargs)
+
+    # Skip empty site names
+    if [ -z "$site_name" ]; then
+        continue
+    fi
+
+    site_path="$sites_dir/$site_name"
+    themes_dir="$site_path/themes/${site_name}_theme"
 
     if [ -d "$themes_dir" ]; then
         echo "Running npm install in $themes_dir"
         cd "$themes_dir"
 
-        # if [ -d node_modules ]; then
-        #   rm -rf node_modules
-        # fi
-
         npm install --silent --no-progress
         npm install nicsdru_unity_theme --silent --no-progress
         npm run build
+    else
+        echo "Themes directory not found for $site_name"
     fi
 done
