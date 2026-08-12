a92cc3edcb0e0ac6a268248dcf6106d0743950ae
omahm <hello@onemanandhismouse.com>
2026-08-12 14:44:54 +0100

    MAES-7 ~ Add update config

--- project/sites/imtac/settings.php
+++ project/sites/imtac/settings.php
@@ -7,6 +7,8 @@ $subsite_id = basename(__DIR__);
 
 include $app_root . '/sites/site.settings.php';
 
+$settings['hash_salt'] = getenv('HASH_SALT');
+
 if (getenv('IS_DDEV_PROJECT')) {
   $databases['default']['default']['database'] = $subsite_id;
 }
